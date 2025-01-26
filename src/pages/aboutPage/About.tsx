1dd03c80fa95ca561fa0ca1a4eea514de7c3f5ff
Juswa <jgonobsit@tfvc.edu.ph>
2025-01-26 11:43:25 +0800

    wadaw

--- src/pages/aboutPage/About.tsx
+++ src/pages/aboutPage/About.tsx
@@ -2,7 +2,7 @@
 
 export default function About() {
   return (
-    <div className="flex flex-col gap-10 items-center  pt-[350px]">
+    <div className="flex flex-col px-[24px] gap-10 items-center  pt-[350px]">
         <h1 className="text-5xl">Built this to practice basic typescript also some basic fundamentals. no database</h1>
         <p className="text-xl">Gono</p>
     </div>
